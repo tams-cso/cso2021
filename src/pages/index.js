5d61db81e89afce9ceca5ca1f4634cbc14a2e5bb
Anthony Zhou <anthony.zhou03@gmail.com>
2020-07-15 22:06:17 -0500

    Discreet scrollbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,36 @@ const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
     <Container>
-      <Row className="justify-content-center my-3" >
-        <h2 style={{textAlign: "center"}}>events & updates</h2>
-      </Row>
-      <Row>
+      <Row style={{minHeight: "90vh"}}>
         <Col>
+        <h2 style={{ textAlign: "center" }}>events & updates</h2>
+
           <p>
             08/19 + GA how 2 code: for beginners
           </p>
           <p>
-           09/05-07 + Hack-a-thon
+            09/05-07 + Hack-a-thon
           </p>
           <p>
 
           </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <h4>Hackathon winners</h4>
+          <p>Thing one - 3rd place, HackMIT</p>
+          <p>Thing one - 3rd place, HackMIT</p>
+          <p>Thing one - 3rd place, HackMIT</p>
+        </Col>
+        <Col md={6}>
+          <h4>Volunteer opportunities</h4>
+          <p>Web Development</p>
+          <p>Good Samaritan</p>
+        </Col>
+        <Col md={6}>
+          <h4>Club Shirt</h4>
+          <p></p>
         </Col>
       </Row>
     </Container>
